37613dbf91b9a922bd1ebf300a1d1a02382c50c2
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-05-23 09:06:14 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/SW1992/pen/Myqary.
+
+ Local Weather App. This is a Free Code Camp Zipline Project. https://www.freecodecamp.com/challenges/show-the-local-weather  (Not yet fixed for Chrome)
---
+++ index.js
@@ -0,0 +1,242 @@
+/* Works in Edge & Firefox, 
+not yet fixed for Chrome */
+$(document).ready(function() {
+  init();
+});
+
+function getUserLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showUserPosition, errorHandler);
+  } else {
+    alert("Geolocation is not supported by your browser.");
+  }
+}
+
+function showUserPosition(position) {
+  var lat = position.coords.latitude;
+  var lon = position.coords.longitude;
+  var appId = "585bc5da6feebc5fef1125a42387df28";
+  var units = "metric";
+  var url = "http://api.openweathermap.org/data/2.5/weather?";
+  var custom = "lat=" + lat + "&lon=" + lon + "&APPID=" + appId + "&units=" + units;
+  url += custom;
+  /* give us our custom url, for api call */
+  $.ajax({
+
+    type: 'GET',
+
+    url: url,
+
+    error: function(xhr, status, error) {
+      if (status === "timeout") {
+        alert(xhr.status + ": " + error);
+      } else if (status === "error") {
+        alert(xhr.status + ": " + error);
+      } else if (status === "abort") {
+        alert(xhr.status + ": " + error);
+      } else if (status === "parsererror") {
+        alert(xhr.status + ": " + error);
+      } else {
+        alert(xhr.status + ": " + error)
+      }
+    },
+
+    success: function(data, status, xhr) {
+      var city = data.name;
+      var country = data.sys.country;
+      var Location = city + ", " + country;
+      var weatherCategory = data.weather[0].main;
+      var weatherID = data.weather[0].id;
+      var temp = Math.trunc(data.main.temp);
+      /* get data from server, store in variables */
+      $(".location").html('<p>' + Location + '</p>');
+      /* set location info */
+      convert(temp);
+      changeBackground(weatherCategory);
+      icons(weatherID);
+      /* pass required data to other functions */
+    }
+  }); 
+}
+
+function errorHandler(error) {
+  if (error.code === error.PERMISSION_DENIED) {
+    alert("Error, permission to access user's location was denied.");
+  } else if (error.code === error.POSITION_UNAVAILABLE) {
+    alert("Error, access to location information is unavailable.");
+  } else if (error.code === error.TIMEOUT) {
+    alert("Error, The request to get user location timed out.");
+  } else if (error.code === error.UNKNOWN_ERROR) {
+    alert("An unknown error occurred.");
+  }
+}
+
+function convert(celsius) {
+  var C = celsius;
+  var F = C * 9 / 5 + 32;
+  $(".temperature").html("<p>" + C + " °C" + "</p>");
+  /* display temperature in celsius by default*/
+  $(".celsius").click(function() {
+    $(".temperature").html("<p>" + C + " °C" + "</p>");
+  });
+  $(".fahrenheit").click(function() {
+    $(".temperature").html("<p>" + F + " °F" + "</p>")
+  });
+  /* convert temperature's html depending on unit measurement choice */
+}
+
+function changeBackground(weather) {
+  var imgUrl = "";
+  if (weather === "Thunderstorm") {
+    imgUrl = "https://pixabay.com/static/uploads/photo/2015/11/14/22/43/flash-1043778_960_720.jpg"
+
+    $(".description").html("<p>" + weather + "</p>")
+    $("body").css('background-image', 'url(' + imgUrl + ')')
+  } else if (weather === "Drizzle") {
+    imgUrl =  "https://pixabay.com/static/uploads/photo/2015/08/03/22/25/rain-874041_960_720.jpg"
+
+    $(".description").html("<p>" + weather + "</p>")
+    $("body").css('background-image', 'url(' + imgUrl + ')')
+  } else if (weather === "Rain") {
+    imgUrl = "https://pixabay.com/static/uploads/photo/2015/07/02/10/45/raindrops-828954_960_720.jpg"
+
+    $(".description").html("<p>" + weather + "</p>")
+    $("body").css('background-image', 'url(' + imgUrl + ')')
+  } else if (weather === "Snow") {
+    imgUrl = "https://pixabay.com/static/uploads/photo/2012/03/01/01/34/winter-20234_960_720.jpg"
+
+    $(".description").html("<p>" + weather + "</p>")
+    $("body").css('background-image', 'url(' + imgUrl + ')')
+  } else if (weather === "Atmosphere") {
+    imgUrl = "https://pixabay.com/static/uploads/photo/2016/01/19/15/20/forest-1149222_960_720.jpg"
+
+    $(".description").html("<p>" + weather + "</p>")
+    $("body").css('background-image', 'url(' + imgUrl + ')')
+  } else if (weather === "Clear") {
+    imgUrl = "https://pixabay.com/static/uploads/photo/2012/03/04/00/01/background-21717_960_720.jpg"
+
+    $(".description").html("<p>" + weather + "</p>")
+    $("body").css('background-image', 'url(' + imgUrl + ')')
+  } else if (weather === "Clouds") {
+    imgUrl = "https://pixabay.com/static/uploads/photo/2016/03/04/22/02/clouds-1236768_960_720.jpg"
+
+    $(".description").html("<p>" + weather + "</p>")
+    $("body").css('background-image', 'url(' + imgUrl + ')')
+  } else if (weather === "Extreme") {
+    imgUrl = "https://pixabay.com/static/uploads/photo/2013/02/25/16/00/key-west-86025_960_720.jpg"
+
+    $(".description").html("<p>" + weather + "</p>")
+    $("body").css('background-image', 'url(' + imgUrl + ')')
+  } else {
+    imgUrl = "https://pixabay.com/static/uploads/photo/2016/02/11/23/01/clouds-1194915_960_720.jpg"
+
+    $(".description").html("<p>" + weather + "</p>")
+    $("body").css('background-image', 'url(' + imgUrl + ')')
+  }
+}
+
+function icons(id) {
+  switch (id) {
+    case 200:
+    case 201:
+    case 202:
+    case 210:
+    case 211:
+    case 212:
+    case 221:
+    case 230:
+    case 231:
+    case 232:
+      $(".icons").attr("src", "http://openweathermap.org/img/w/11d.png")
+      break;
+    case 300:
+    case 301:
+    case 302:
+    case 310:
+    case 311:
+    case 312:
+    case 313:
+    case 314:
+    case 321:
+      $(".icons").attr("src", "http://openweathermap.org/img/w/09d.png");
+      break;
+    case 500:
+    case 501:
+    case 502:
+    case 503:
+    case 504:
+    case 511:
+    case 520:
+    case 521:
+    case 522:
+    case 531:
+      $(".icons").attr("src", "http://openweathermap.org/img/w/10d.png");
+      break;
+    case 600:
+    case 601:
+    case 602:
+    case 611:
+    case 612:
+    case 615:
+    case 616:
+    case 620:
+    case 621:
+    case 622:
+      $(".icons").attr("src", "http://openweathermap.org/img/w/13d.png");
+      break;
+    case 701:
+    case 711:
+    case 721:
+    case 731:
+    case 741:
+    case 751:
+    case 761:
+    case 762:
+    case 771:
+    case 781:
+      $(".icons").attr("src", "http://openweathermap.org/img/w/50d.png");
+      break;
+    case 800:
+      $(".icons").attr("src", "http://openweathermap.org/img/w/01d.png");
+      break;
+    case 801:
+      $(".icons").attr("src", "http://openweathermap.org/img/w/02d.png");
+      break;
+    case 802:
+      $(".icons").attr("src", "http://openweathermap.org/img/w/03d.png");
+      break;
+    case 803:
+    case 804:
+      $(".icons").attr("src", "http://openweathermap.org/img/w/04d.png");
+      break;
+    case 900:
+    case 901:
+    case 903:
+    case 904:
+    case 905:
+    case 906:
+$(".icons").attr("src", "http://openweathermap.org/img/w/11d.png")
+      break;
+    case 950:
+    case 951:
+    case 952:
+    case 953:
+    case 954:
+    case 955:
+    case 956:
+    case 957:
+    case 958:
+    case 959:
+    case 960:
+    case 961:
+    case 962:
+         $(".icons").attr("src", "http://openweathermap.org/img/w/50d.png");
+      break;
+    default:
+      $(".icons").attr("src", "http://openweathermap.org/img/w/03d.png")
+  }
+}
+
+function init() {
+  getUserLocation();
+}
